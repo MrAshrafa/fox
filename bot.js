15026be0a31b870105d7d043b330f9980a7ea64b
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-28 18:23:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '='
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Fox ***  `,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`Fox ✓`,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -30,6 +30,15 @@ client.user.setGame(`Fox ***  `,"https://www.twitch.tv/dggamingbot")
   console.log('')
 });
  
-
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "welcome");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  });
+})
  
 client.login(process.env.BOT_TOKEN);
