31cf1e531d8f8fd4f34f3bc26cf65ba8b71b6b95
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-28 18:12:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,30 +54,6 @@ client.on('guildMemberAdd', member => {
       channel.sendEmbed(embed);
     });
     
- client.on('message', message => {
-    if (message.content.includes('discord.gg')){
-                        if(!message.channel.guild) return message.reply ('')
-                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
-       message.channel.send('ban <@' + message.author.id + '>')
-       message.delete()
-       }
-    }
-          if (message.content.startsWith("ban")) {
-             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
-             var member= message.mentions.members.first();
-             member.ban().then((member) => {
-                 message.channel.sendMessage("", {embed: {
-                 author: {
-                 },
-                 title: 'بسبب النشر ' + member.displayName + ' تم حظر',
-                 color: 490101,
-                 }
-               });
-           }
-         )
-       }
-   });
-   
 client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
