51235e93b1cc801920165bf58aed3ed8cab60d56
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-29 14:06:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,22 +85,7 @@ client.on('guildMemberAdd', member => {
     });
     
 
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
-});
+
 
 
 
