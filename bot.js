61948d09cc76265c47b30eb62ac678f58b5c79e7
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-29 18:19:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ message.channel.send({embed:embed}).then(message => message.delete(5000));
 }
 });
   client.on('message',message =>{
-      var prefix = "#";
+      var prefix = "=";
  if(message.content.split(' ')[0].toLowerCase() == prefix + 'انفايت') {
 let guild = message.guild
 var codes = [""]
@@ -210,115 +210,5 @@ client.on('message' , async (message) => {
 });
 
 
-
-client.on('message', message => {
-var prefix = "."
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
- if(message.content.split(' ')[0].toLowerCase() == prefix + 'channelinfo') {
-  if(message.channel.permissionsFor(message.client.user).has('EMBED_LINKS') == false) return message.reply('sorry but I cannot send Embed Links for this channel... check my permissions and try again!');
-  var kakchannel = args.slice(1).join(' ');
-  if(!kakchannel) return message.reply('please, specify a channel to get info...');
-
-  var achannel = message.guild.channels.find('name', kakchannel);
-  if(!achannel) return message.reply("i didn't find no channel with tha name! Try again later 👎");
-
-  var channelCreated = achannel.createdAt.toString().split(' ');
-       const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-  const embed = new Discord.RichEmbed()
-  .setColor(0xFF8DFD)
-  .setImage()
-  .setThumbnail('http://cdn.onlinewebfonts.com/svg/img_323299.png')
-  .setURL('')
-  .addField(`Channel Name`, `${achannel.name}`, true)
-  .addField(`Channel ID`, `${achannel.id}`, true)
-  .addField(`Channel Position`, `${achannel.position}`, true)
-  .addField(`Channel Members`, `${achannel.members.size} have acess`, true)
-  .addField(`Channel Type`, `${achannel.type}`, true)
-  .addField(`Channel Topic`, `${achannel.topic}`, true)
-  .addField(`Channel Created At`, `${createdAt.toFixed(0)} Days ago `)
-
-  message.channel.send({embed})
-}
-});
-
-
-
-client.on('message' , message => {
-     const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
- if(message.content.split(' ')[0].toLowerCase() == prefix + 'ايدي') {
-     const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-    let user = message.mentions.users.first() || message.author;
-    message.delete();
-   
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-
-    let game;
-    if (user.presence.game === null) {
-        game = 'None.';
-    } else {
-        game = user.presence.game.name;
-    }
-    let messag;
-    if (user.lastMessage === null) {
-        messag = 'None.';
-    } else {
-        messag = user.lastMessage;
-    }
-    let status;
-    if (user.presence.status === 'online') {
-        status = 'Online';
-    } else if (user.presence.status === 'dnd') {
-        status = 'DND';
-    } else if (user.presence.status === 'idle') {
-        status = 'Idle';
-    } else if (user.presence.status === 'offline') {
-        status = 'Offline';
-    }
-    if (user.presence.status === 'offline') {
-        stat = 0x000000;
-    } else if (user.presence.status === 'online') {
-        stat = 0x00AA4C;
-    } else if (user.presence.status === 'dnd') {
-        stat = 0x9C0000;
-    } else if (user.presence.status === 'idle') {
-        stat = 0xF7C035;
-    }
-    moment.locale('En-ly');
-                    message.guild.fetchInvites().then(invs => {
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let Invites = invs.filter(i => i.inviter.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-    const embed = new Discord.RichEmbed()
-
-  
-  .addField('Discord Info : ', `Name : ${user.username}\n Discriminator: #${user.discriminator}\nID : ${user.id} \nJoinedDiscord : ${moment(heg.createdTimestamp).fromNow()}\nBot :  ${user.bot}\nPlaying : ${game}\nStatus : ${status}`,true)
-  .addField('Server Info :', `LastMessage : ${messag}\nJoined :  ${moment(h.joinedAt).fromNow()} \n Invites :  ${inviteCount} Invite(s) \nRoles : `+message.guild.members.get(user.id).roles.array(role => role.name).slice(1).join(', '))
-  .setAuthor(`${user.username}`, user.displayAvatarURL)
-  .setColor('#36393e')
-    .setThumbnail(user.displayAvatarURL)
-    message.channel.send({embed})
-  .catch(e => logger.error(e));
- })
-}
- });
 
 client.login(process.env.BOT_TOKEN);
