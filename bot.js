8ccc0bc74bddec1a5b6c575f87a493c3ae9d7d5b
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-29 14:07:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,36 +30,6 @@ client.user.setGame(`Fox ✓`,"https://www.twitch.tv/dggamingbot")
   console.log('')
 });
  
-client.on('message', message => {
-  if (message.content === '=help') {
-	  
-    message.author.send(`
-«======================»
-  prefix : {=}
-«======================»
-  bot command
-«======================»
-=avater {حتة يضهر صورا حسابك}
-=new {حتة تغتح تذكرة}
-=clock {حتة يضهر لك وقت}
-=bot {ليضهر اك معلومات بوت}
-
-`);
-	message.react('✉');
-  }
-});
-
-
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-}
 
 
 client.on('guildMemberAdd', member => {
